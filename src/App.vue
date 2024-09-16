06a3d601e3c589e5211b56e0b2d62c00707a981f
shun-sa <shun.sa-0520@outlook.jp>
2024-09-17 08:27:29 +0900

    ts-ignore

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+//@ts-ignore
 import { Authenticator } from "@aws-amplify/ui-vue";
   import "@aws-amplify/ui-vue/styles.css";
 
+  //@ts-ignore
   import { Amplify } from 'aws-amplify';
+  //@ts-ignore
   import outputs from '../amplify_outputs.json';
 
   Amplify.configure(outputs);
