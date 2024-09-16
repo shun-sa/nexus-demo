fabfc9e10ddb2c5000272caaeb16101706ce2920
shun-sa <shun.sa-0520@outlook.jp>
2024-09-17 08:17:47 +0900

    認証画面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import outputs from '../amplify_outputs.json';
+
+  Amplify.configure(outputs);
 </script>
 
 <template>
-  <main>
-    <Todos />
-  </main>
+  <authenticator>
+    <main>
+      <Todos/>
+    </main>
+  </authenticator>
 </template>
 
