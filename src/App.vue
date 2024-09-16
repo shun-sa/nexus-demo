8f3d27137b60a7b2ea8b49976b8a5b7f8fbe83dc
shun-sa <shun.sa-0520@outlook.jp>
2024-09-17 08:34:39 +0900

    認証外し。

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
-//@ts-ignore
-import { Authenticator } from "@aws-amplify/ui-vue";
-  import "@aws-amplify/ui-vue/styles.css";
+// //@ts-ignore
+// import { Authenticator } from "@aws-amplify/ui-vue";
+//   import "@aws-amplify/ui-vue/styles.css";
 
-  //@ts-ignore
-  import { Amplify } from 'aws-amplify';
-  //@ts-ignore
-  import outputs from '../amplify_outputs.json';
+//   //@ts-ignore
+//   import { Amplify } from 'aws-amplify';
+//   //@ts-ignore
+//   import outputs from '../amplify_outputs.json';
 
-  Amplify.configure(outputs);
+  //Amplify.configure(outputs);
 </script>
 
 <template>
-  <authenticator>
+  <!-- <authenticator> -->
     <main>
       <Todos/>
     </main>
-  </authenticator>
+  <!-- </authenticator> -->
 </template>
 
